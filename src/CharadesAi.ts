12411949a551dba86ebf44f1943a278641a2e502
fstrobl <felix.strobl@axelspringer.com>
2024-03-09 02:40:27 +0100

    add keywords endpoint

--- src/CharadesAi.ts
+++ src/CharadesAi.ts
@@ -4,11 +4,13 @@ const OPENAI_MODEL = "gpt-3.5-turbo";
 
 export class CharadesAi {
   private openai: OpenAI;
+
   constructor() {
     this.openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
     });
   }
+
   async *getRecommendationsStream() {
     const stream = await this.openai.chat.completions.create({
       model: OPENAI_MODEL,
@@ -21,6 +23,23 @@ export class CharadesAi {
       ],
       stream: true,
     });
+
+    for await (const chunk of stream) {
+      yield chunk.choices[0]?.delta?.content || "";
+    }
+  }
+  async *getKeywordsStream(userInput: string) {
+    const stream = await this.openai.chat.completions.create({
+      model: OPENAI_MODEL,
+      messages: [
+        {
+          role: "user",
+          content: `Create a list of keywords used for a game of charades. The keywords should be related to this topic: ${userInput}`,
+        },
+      ],
+      stream: true,
+    });
+
     for await (const chunk of stream) {
       yield chunk.choices[0]?.delta?.content || "";
     }
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,20 @@ const app = new Hono().basePath("/api");
 
 app.use(logger());
 
+app.post("/keywords", async (c) => {
+  const body = await c.req.json();
+
+  if (typeof body.userInput !== "string") {
+    return c.text("Invalid input", 400);
+  }
+  return streamText(c, async (stream) => {
+    const keywordGenerator = charadesAi.getKeywordsStream(body.userInput);
+    for await (const keyword of keywordGenerator) {
+      await stream.write(keyword);
+    }
+  });
+});
+
 app.get("/recommendations", async (c) => {
   return streamText(c, async (stream) => {
     for await (const recommendation of charadesAi.getRecommendationsStream()) {
